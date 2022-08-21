b933918b0882a983cff6dee2a0ee7611ef8a18f4
joeephwild <josephomotade0@gmail.com>
2022-08-21 13:53:18 +0000

    commit

---
+++ src/components/Categories.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import banner2 from '../assets/B-img2.jpg';
+
+const Categories = () => {
+  return (
+    <div className='w-full  h[100vh] space-y-7 overflow-x-hidden'>
+        <div className="md:flex block  md:py-9 md:pl-7 md:space-x-10 md:bg-[#fc81bf] mt-6 ">
+        <div className='bg-[#fc75b8] md:ml-12 flex flex-col items-center'>
+            <div className="text-4xl font-extrabold text-[#0f0d0d] text-center w-[25rem] mb-7">Summer’s brightest new drinks</div>
+            <div className='text-3xl w-[35rem] font-bold'>Introducing the Pineapple Passionfruit Starbucks Refreshers® beverage and the Starbucks® Paradise Drink with creamy coconutmilk.</div>
+            <button className='text-[#161515] text-base mt-4 border-2 border-[#0000] capitalize font-bold rounded-full px-3 py-2'>Learn More</button>
+        </div>
+        <img src={banner2} alt="banner" className='md:w-[460px] object-contain'/>
+    </div>
+    </div>
+  )
+}
+
+export default Categories
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FaMapMarkerAlt } from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between border-b-2 border-gray-300 items-center w-full p-7'>
+        <div className='flex items-center '>
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" className='w-12 h-12 object-contain' />
+        <div className='md:flex hidden ml-6 space-x-4 font-bold cursor-pointer'>
+            <div className='text-base hover:text-green-700'>MENU</div>
+            <div className='text-base hover:text-green-700'>REWARDS</div>
+            <div className='text-base hover:text-green-700'>GIFT CARDS</div>
+        </div>
+        </div>
+        
+        <div className='md:flex hidden space-x-6 cursor-pointer items-center'>
+            <span className='flex hover:text-green-700 space-x-2 items-center'>
+                <FaMapMarkerAlt size={25} />
+                <span className='font-bold '>Find a store</span>
+            </span>
+            <div className='flex space-x-4'>
+            <button className='text-[#000000de] border-2 border-[#000000de]  rounded-full px-3 py-2'>Sign in</button>
+            <button className='bg-[#000000de] text-[#fafafa] px-3 py-2 rounded-full'>Join now</button>
+        </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar

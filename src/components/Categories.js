f251469d0a6bd6e2ead8151da727657362b9b2c7
joeephwild <josephomotade0@gmail.com>
2022-08-21 13:59:55 +0000

    commit

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,13 +4,13 @@ import banner2 from '../assets/B-img2.jpg';
 const Categories = () => {
   return (
     <div className='w-full  h[100vh] space-y-7 overflow-x-hidden'>
-        <div className="md:flex block  md:py-9 md:pl-7 md:space-x-10 md:bg-[#fc81bf] mt-6 ">
-        <div className='bg-[#fc75b8] md:ml-12 flex flex-col items-center'>
+        <div className="md:flex block  md:py-9 md:pl-7 md:space-x-10 md:bg-[#f19cbb] mt-6 ">
+        <div className='bg-[#f19cbb] md:ml-12 flex flex-col items-center'>
             <div className="text-4xl font-extrabold text-[#0f0d0d] text-center w-[25rem] mb-7">Summer’s brightest new drinks</div>
             <div className='text-3xl w-[35rem] font-bold'>Introducing the Pineapple Passionfruit Starbucks Refreshers® beverage and the Starbucks® Paradise Drink with creamy coconutmilk.</div>
             <button className='text-[#161515] text-base mt-4 border-2 border-[#0000] capitalize font-bold rounded-full px-3 py-2'>Learn More</button>
         </div>
-        <img src={banner2} alt="banner" className='md:w-[460px] object-contain'/>
+        <img src={banner2} alt="banner" className='md:w-[360px] h-[360px] object-contain'/>
     </div>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-between border-b-2 border-gray-300 items-center w-full p-7'>
         <div className='flex items-center '>
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" className='w-12 h-12 object-contain' />
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" alt="logo" className='w-12 h-12 object-contain' />
         <div className='md:flex hidden ml-6 space-x-4 font-bold cursor-pointer'>
             <div className='text-base hover:text-green-700'>MENU</div>
             <div className='text-base hover:text-green-700'>REWARDS</div>

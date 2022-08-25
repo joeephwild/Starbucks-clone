f6c16eb193e1e15d56de732b1beb8a3ab2dfde3a
joeephwild <josephomotade0@gmail.com>
2022-08-25 06:25:02 +0000

    commit

--- src/components/FindStore/Map.js
+++ src/components/FindStore/Map.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
 
+const style = {
+    wrapper: `flex-1 h-[100vh] overflow-hidden`,
+  }
+
+  mapboxgl.accessToken = ('pk.eyJ1Ijoiam9zZXBod2ViIiwiYSI6ImNsM3pjMnp5djEyMXkzYnF0NmZlYWhvOGgifQ.-h4WEnN0qYj2P0U0NQkc1Q')
 const Map = () => {
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/josephweb/cl3zg581z000114o51w32qnct',
+          
+    
+          center: [-99.29011, 39.39172],
+          zoom: 3,
+        })
+    
+      }, [])
+    
   return (
-    <div>Map</div>
+    <div className={style.wrapper} id='map'/>
   )
 }
 
--- src/pages/FindStore.js
+++ src/pages/FindStore.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Location from '../components/FindStore/Location'
 import Map from '../components/FindStore/Map'
+import Footer from '../components/HomeSreen/Footer'
+import Navbar from '../components/HomeSreen/Navbar'
 
 const FindStore = () => {
   return (
-    <div className='grid grid-cols-2 '>
+    <div className='w-full'>
+        <Navbar />
+        <div className='grid grid-cols-1 gap-8 md:grid-cols-2 '>
         <Location />
         <Map />
+        </div>
+        <Footer />
     </div>
   )
 }

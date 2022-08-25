267ce6a369616af01e92bbc6a5eb836859f42e90
joeephwild <josephomotade0@gmail.com>
2022-08-25 06:46:24 +0000

    commit

--- src/components/FindStore/Map.js
+++ src/components/FindStore/Map.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 
 const style = {
-    wrapper: `flex-1 h-[100vh] overflow-hidden`,
+    
+    wrapper: `flex-1 h-[100vh] relative overflow-hidden`,
   }
 
   mapboxgl.accessToken = ('pk.eyJ1Ijoiam9zZXBod2ViIiwiYSI6ImNsM3pjMnp5djEyMXkzYnF0NmZlYWhvOGgifQ.-h4WEnN0qYj2P0U0NQkc1Q')
 const Map = () => {
+    const [count, setCount] = useState(8)
+
     useEffect(() => {
         const map = new mapboxgl.Map({
           container: 'map',
@@ -14,13 +17,20 @@ const Map = () => {
           
     
           center: [-99.29011, 39.39172],
-          zoom: 3,
+          zoom: {count},
         })
     
       }, [])
     
   return (
-    <div className={style.wrapper} id='map'/>
+    <div>
+        <div className={style.wrapper} id='map'/>
+        <div className="flex justify-between items-center">
+            <button className="bg-green-600 font-bold text-white px-4 py-2 roundedlg" onClick={() => setCount(count + 1)}>+</button>
+            <button className="bg-green-600 font-bold text-white px-4 py-2 roundedlg" onClick={() => setCount(count - 1)}>-</button>
+        </div>
+    </div>
+    
   )
 }
 

086af5dc218688a4f5462cdd02bb153d2df0c333
joeephwild <josephomotade0@gmail.com>
2022-10-09 10:00:08 +0100

    commiting

--- src/components/Account/JoinNowForm.js
+++ src/components/Account/JoinNowForm.js
@@ -1,121 +1,16 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Checkbox} from '@chakra-ui/react'
-import styled from '@emotion/styled'
 
-
-const style = {
-   wrapper: "md:max-w-[100vw] max-w-[100vw] mb-[60px] flex-col md:mt-16 flex   ",
-   formWrap: ' md:max-w-[600px] px-[40px] md:px-[80px] mx-auto  py-[98px]  rounded-lg shadow-md shadow-slate-300 space-y-8',
-   input: 'md:w-[30rem] w-[25rem]  px-4 border-[#00000094] mt-6 border-2 rounded-lg h-16'
-}
-
-const JormForm = ({ handleSubmit, setPassword, setFirstName, setLastName, setEmail, email, password, firstName, lastName }) => {
-  return (
-    <>
-       <div className={style.wrapper}>
-         
-         <form onSubmit={handleSubmit} className={style.formWrap}>
-            <p className="text-sm mb-8">* indicates required field</p>
-            <label htmlFor='name' className="text-xl font-bold mt-6">Personal Information</label>
-                <input 
-                value={firstName}
-                onChange={e => setFirstName(e.target.value)}
-                    type="text" 
-                     placeholder='*First name' 
-                     className={style.input} />
-                  <input 
-                  value={lastName}
-                  onChange={e => setLastName(e.target.value)}
-                    type="text" 
-                     placeholder='*Last name' 
-                     className={style.input} />
-
-                     {/* account security section */}
-                  <div className='flex mt-28 flex-col mb-16 mx-auto'>
-                  <label htmlFor='name' className="text-xl font-bold mt-6">Account Security</label>
-
-                     <input 
-                     value={email}
-                     onChange={e => setEmail(e.target.value)}
-                      type="email" 
-                      placeholder='*Email address' 
-                      required
-                      className={style.input} />
-                     <span className='text-sm font-semibold mr-[20%] text-gray-500'>This will be your username</span>
-                   <input 
-                   value={password}
-                   onChange={e => setPassword(e.target.value)}
-                   required
-                    type="password" 
-                     placeholder='*Password' 
-                     className={style.input} />
-                     <span className='text-sm font-semibold md:w-[28rem] text-center text-gray-500'>Create a password 8 to 25 characters long that includes at least 1 uppercase and 1 lowercase letter, 1 number and 1 special character like an exclamation point or asterisk.</span>
-                  </div>
-                
-               <div className='flex flex-col mx-auto mt-16'>
-                <span className='text-[#00a862] font-bold mr-[20px] text-lg underline mb-7'>Already have a Starbucks gift card?</span>
-                <span className='text-sm text-[#00000094] mr-[20px] leading-[.1em] uppercase font-bold mb-[2.4rem]'>COLLECT MORE STARS & EARN REWARDS</span>
-                <span className='text-sm text-[#000000de] font-semibold block mb-[2.4rem]'>Email is a great way to know about offers and what’s new from Starbucks.</span>
-
-                <div className='flex space-x-3 py-[10px] text-[#00000094] items-center'>
-                <Checkbox 
-                size="lg"
-                borderRadius="3xl"
-                colorScheme="green"
-                className='text-[#00000094] text-sm' 
-                animation="ease-in">Yes, I’d like email from Starbucks</Checkbox>
-                  
-                </div>
-                <p className='text-[#000000de] text-center mt-3 text-sm'>Know about initiatives, announcements and product offers.</p>
-               </div>
-
-                    {/* terms of use */}
-                     <span className='text-[#00000094] py-16 text-lg'>TERMS OF USE</span>
-                     <div className='flex items-center my-[60px] px-5 space-y-7 mx-auto'>
-                
-                      <Checkbox
-                         size="lg"
-                        iconColor='black'
-                        iconSize="30px"
-                        borderRadius="3xl"
-                       colorScheme="green"
-                       animation="ease-in" >
-                       </Checkbox>
-                       <span className='text-[#00000094] my-16 text-sm'>I agree to <span className='text-[#00a862] font-bold mr-[20px] text-sm underline mb-7'> the 
-                        Starbucks® Rewards Terms</span>opens in new window and the<span className='text-[#00a862] font-bold mr-[20px] text-sm underline mb-7'>Starbucks Card Terms</span>
-                        opens in new window and have read the<span className='text-[#00a862] font-bold text-sm underline'>Starbucks Privacy Statement</span>
-                        opens in new window</span>  
-               </div>
-               <button className="text-xl font-bold mb-[50px] bg-[#184733] ml-[50%] rounded-full px-5 py-4 text-[#fafafa]">Create account</button>
-=======
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { AiOutlineCheck, AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai'
 
 const style = {}
 
-const JormForm = ({  
-  setPassword, 
-  setFirstName, 
-  setLastName, 
-  setEmail, email, password, firstName, lastName }) => {
-    //importing the needed from react hook form useform
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+const JormForm = ({setPassword, setFirstName, setLastName, setEmail, email, password, firstName, lastName }) => {
     const [type, setType] = useState('password');
     const [icon, setIcon] = useState(AiOutlineEyeInvisible);
 
     //handle password icon toogle
-    const handleToogle = () => {
-      if(type === 'password') {
-        setIcon(AiOutlineEye);
-        setType('text')
-      }
-      else {
-        setIcon(AiOutlineEyeInvisible);
-        setType('password')
-      }
-    }
+    
   return (
     <>
        <div className='max-w-[500px] !mx-auto pb-[4.2rem]'>
@@ -190,7 +85,7 @@ const JormForm = ({
                         {icon}
                       </div>
                    </div>
-                   {errors.{password} && 
+                   {errors.password && 
                    <div>
                     <p>from 8 to 25 characters</p>
                    <p>At least one number</p>
@@ -267,8 +162,6 @@ const JormForm = ({
                <div className='!justify-end mt-[4rem] !flex'>
                <button type='submit' className='bg-[#00754a] py-3 rounded-full text-center px-9 text-xl font-bold shadow-xl shadow-gray-300 transition ease text-[#fff]' >Create account</button>
                </div>
-               
->>>>>>> 49ab28f (login commit)
          </form>
        </div>
     </>
